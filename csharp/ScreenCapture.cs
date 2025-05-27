caaa79f1b0a5e28c19da46b97797b3bdd86b7b24
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-27 21:04:17 +0300

    Update ScreenCapture.cs

--- csharp/ScreenCapture.cs
+++ csharp/ScreenCapture.cs
@@ -1,1 +1,23 @@
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
+public static class ScreenCapture
+{
+    public static byte[] Capture(int width, int height)
+    {
+        using (Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb))
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+            }
+
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            byte[] result = new byte[data.Height * data.Stride];
+            Marshal.Copy(data.Scan0, result, 0, result.Length);
+            bitmap.UnlockBits(data);
+            return result;
+        }
+    }
+}

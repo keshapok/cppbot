5b833b28abfd061397c34969e67bc3578c1fe9ea
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-27 21:04:01 +0300

    Update Win32.cs

--- csharp/Win32.cs
+++ csharp/Win32.cs
@@ -1,1 +1,24 @@
+using System.Runtime.InteropServices;
 
+public static class Win32
+{
+    [DllImport("user32.dll")]
+    public static extern void SetCursorPos(int X, int Y);
+
+    [DllImport("user32.dll")]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+    public const uint KEYEVENTF_KEYUP = 0x0002;
+
+    public static void PressKey(byte vkCode)
+    {
+        keybd_event(vkCode, 0, 0, UIntPtr.Zero);
+        System.Threading.Thread.Sleep(50);
+        keybd_event(vkCode, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+    }
+
+    public static class VirtualKeyCodes
+    {
+        public const byte VK_SPACE = 0x20;
+    }
+}

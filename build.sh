6bc5556c6b0322698dc97ed2eadd253fb22f1f72
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-27 21:05:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+echo "Установка зависимостей..."
+sudo apt update
+sudo apt install -y cmake g++ mingw-w64 libopencv-dev
+dotnet --version || sudo apt install -y dotnet-sdk-8.0
+
+cd cpp
+mkdir -p build && cd build
+cmake ..
+make
+
+cd ../../csharp
+dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained

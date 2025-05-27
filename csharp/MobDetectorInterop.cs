d6713a7e87ea8b4efb2abcb0d343d1c22cdd8bbc
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-27 21:04:39 +0300

    Update MobDetectorInterop.cs

--- csharp/MobDetectorInterop.cs
+++ csharp/MobDetectorInterop.cs
@@ -1,1 +1,12 @@
+using System.Runtime.InteropServices;
 
+public static class MobDetectorInterop
+{
+    [DllImport("CppOpenCvDetector.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int DetectMobs(
+        byte[] imageData,
+        int width,
+        int height,
+        int[] outBuffer,
+        int bufferSize);
+}
